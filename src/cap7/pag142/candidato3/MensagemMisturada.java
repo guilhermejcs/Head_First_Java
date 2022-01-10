a8ecfc11c69492bc849e75d23e1884f3c945cddc
Guilherme Souza <guilhermejcamargos@gmail.com>
2022-01-10 10:40:47 -0300

    Exercícios cap. 6 pag. 122 e cap. 7 pag. 142

---
+++ src/cap7/pag142/candidato3/MensagemMisturada.java
@@ -0,0 +1,46 @@
+package cap7.pag142.candidato3;
+
+class A {
+    int ivar = 7;
+
+    void m1() {
+        System.out.print("A's m1, ");
+    }
+
+    void m2() {
+        System.out.print("A's m2, ");
+    }
+
+    void m3() {
+        System.out.print("A's m3, ");
+    }
+}
+
+class B extends A {
+    void m1() {
+        System.out.print("B's m1, ");
+    }
+}
+
+class C extends B {
+    void m3() {
+        System.out.print("C's m3, " + (ivar + 6));
+    }
+}
+
+class Mixed2 {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A a2 = new C();
+
+        // O código candidato entra aqui (três linhas)
+        a.m1();
+        b.m2();
+        c.m3();
+    }
+}
+/*
+RESPOSTA: A's m1, A's m2, C's m3, 13
+ */
