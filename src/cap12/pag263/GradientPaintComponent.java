49836a1a2278098e9cb61110cbe8bf50607f34f0
Guilherme Jorge Camargos de Souza <servicozuptecnologia740892@userede.com.br>
2022-01-13 18:35:14 -0300

    Exercícios cap. 12

--- src/cap12/pag263/GradientPaintComponent.java
+++ src/cap12/pag263/GradientPaintComponent.java
@@ -1,4 +1,4 @@
-package cap12.pag262;
+package cap12.pag263;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,10 +8,19 @@ public class GradientPaintComponent extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
-        GradientPaint gradient = new GradientPaint(70,70,Color.blue, 150,150,Color.orange);
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red,green,blue);
+
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endColor = new Color(red,green,blue);
+
+        GradientPaint gradient = new GradientPaint(70,70,startColor, 150,150,endColor);
         g2d.setPaint(gradient);
         g.fillOval(70,70,100,100);
-
     }
 
     public static void main(String[] args) {
