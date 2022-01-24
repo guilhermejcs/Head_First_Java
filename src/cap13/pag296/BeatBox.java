bee4f804bdd036da46bc34a53b453e974389ab02
Guilherme Souza <guilhermejcamargos@gmail.com>
2022-01-24 08:58:58 -0300

    Exercícios cap. 13 - BeatBox

--- src/cap13/pag296/BeatBox.java
+++ src/cap13/pag296/BeatBox.java
@@ -1,10 +1,10 @@
 package cap13.pag296;
 
-import javax.sound.midi.Sequence;
-import javax.sound.midi.Sequencer;
-import javax.sound.midi.Track;
+import javax.sound.midi.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class BeatBox {
@@ -81,11 +81,96 @@ public class BeatBox {
         theFrame.setVisible(true);
     } // fecha o método
 
-    private void setUpMidi() {
+    public void setUpMidi() {
         try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // fecha o método
 
-        } catch ()
+    public void buildTrackAndStart(){
+        int[] trackList = null;
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+            int key = instruments[i];
+            for (int j = 0; j < 16; j++) {
+                JCheckBox jc = (JCheckBox) checkboxList.get(j + (16 * i));
+                if (jc.isSelected()) {
+                    trackList[j] = key;
+                } else {
+                    trackList[j] = 0;
+                }
+            } // fecha o loop interno
+            makeTracks(trackList);
+            track.add(makeEvent(176,1,127,0,16));
+        } // fecha o loop externo
+
+        track.add(makeEvent(192, 9, 1, 0, 15));
+
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // fecha o método buildTrackAndStart
+
+    public class MyStartListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            buildTrackAndStart();
+        }
+    } // fecha a classe interna
+
+    public class MyStopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            sequencer.stop();
+        }
+    } // fecha a classe interna
+
+    public class MyUpTempoListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float)(tempoFactor *1.03));
+        }
+    } // fecha a classe interna
+
+    public class MyDownTempoListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float)(tempoFactor*.97));
+        }
+    } // fecha a classe interna
+
+    public void makeTracks(int[] list) {
+        for (int i = 0; i < 16; i++) {
+            int key = list[i];
+            if (key != 0) {
+                track.add(makeEvent(144, 9, key, 100, i));
+                track.add((makeEvent(128, 9, key, 100, i + 1)));
+            }
+        }
     }
 
-
-}
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return event;
+    }
+} // fecha a classe
