2c1f9c42e48eff882644abbf50448d547d9c5fdc
Guilherme Souza <guilhermejcamargos@gmail.com>
2022-02-02 20:51:23 -0300

    Exercícios cap. 15

---
+++ src/cap15/pag365/SimpleChatClient.java
@@ -0,0 +1,91 @@
+package cap15.pag365;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SimpleChatClient {
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args) {
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame("Ludicrously Simpe Chat Client");
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        setUpNetworking();
+
+        Thread readThread = new Thread(new IncomingReader());
+        readThread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+    } // fecha go
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } // fecha setUpNetworking
+    }
+
+    private class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        } // fecha a classe interna
+    }
+
+    public class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                } // fecha while
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } // fecha run
+    } // fecha classe interna
+} // fecha a classe externa
